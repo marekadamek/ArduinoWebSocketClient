ab021883aeb612c0be4bbbb65168748ad2f27d83
Marek Adamek <wycieruch@gmail.com>
2014-09-19 20:42:44 +0900

    astyle formatting

--- ArduinoWebSocketClient.h
+++ ArduinoWebSocketClient.h
@@ -5,35 +5,36 @@
 #include "Arduino.h"
 #include <Client.h>
 
-class ArduinoWebSocketClient {
-	public:
-	static const int HEARTBEAT_INTERVAL = 5000;
-	static const unsigned long HANDSHAKE_TIMEOUT = 30000;
-	static const int MAX_PAYLOAD = 125;
+class ArduinoWebSocketClient
+{
+public:
+    static const int HEARTBEAT_INTERVAL = 5000;
+    static const unsigned long HANDSHAKE_TIMEOUT = 30000;
+    static const int MAX_PAYLOAD = 125;
 
-	typedef void (*OnMessageHandler)(ArduinoWebSocketClient* client, char* message, int size);
-	
-	ArduinoWebSocketClient(bool wifi = false);
-	~ArduinoWebSocketClient();
+    typedef void (*OnMessageHandler)(ArduinoWebSocketClient* client, char* message, int size);
 
-	bool connect(char* hostname, int port, char* websocketUri);
+    ArduinoWebSocketClient(bool wifi = false);
+    ~ArduinoWebSocketClient();
+
+    bool connect(char* hostname, int port, char* websocketUri);
     bool connected();
     void disconnect();
-	void monitor();
-	bool send(char *payload, int size);
-	void setMessageHandler(OnMessageHandler handler);
-	
-	private:
-	Client* client;
+    void monitor();
+    bool send(char *payload, int size);
+    void setMessageHandler(OnMessageHandler handler);
+
+private:
+    Client* client;
 
     OnMessageHandler handler;
     unsigned long lastHeartbeat;
     char* hostname;
     int port;
     char* websocketUri;
-    
+
     bool sendHandshake();
-	int read(char* buffer);
+    int read(char* buffer);
 };
 
 #endif
--- examples/ArduinoWebSocketClientExample.ino
+++ examples/ArduinoWebSocketClientExample.ino
@@ -5,71 +5,76 @@
 
 #include "ArduinoWebSocketClient.h"
 
-char* ssid = "ssid";     
+char* ssid = "ssid";
 char* pass = "pass";
 
 char* host = "ip_or_hostname";
 int port = 80;
 char* websocketUri = "/websocket";
 
-void handler(ArduinoWebSocketClient* client, char *data, int size) {
-  Serial.print("Received data of size: ");
-  Serial.println(size);
-  Serial.print("Data: ");
-  Serial.println(data);
+void handler(ArduinoWebSocketClient* client, char *data, int size)
+{
+    Serial.print("Received data of size: ");
+    Serial.println(size);
+    Serial.print("Data: ");
+    Serial.println(data);
 }
 
 ArduinoWebSocketClient client(true);
 
-void setup() {
-  Serial.begin(9600);
-  SPI.begin();
-  
-  while(WiFi.status() == WL_NO_SHIELD);
+void setup()
+{
+    Serial.begin(9600);
+    SPI.begin();
 
-  // attempt to connect to Wifi network:
-  int status = WL_IDLE_STATUS;
-  while ( status != WL_CONNECTED) { 
-    Serial.print("Attempting to connect to Network named: ");
-    Serial.println(ssid);
+    while(WiFi.status() == WL_NO_SHIELD);
 
-    status = WiFi.begin(ssid, pass);
-    delay(5000);
-  } 
- 
-  printWifiStatus();
- 
-  Serial.print("connecting to websocket... ");
-  client.setMessageHandler(handler);
-  while(!client.connect(host, port, websocketUri));
-  Serial.println("OK");
+    // attempt to connect to Wifi network:
+    int status = WL_IDLE_STATUS;
+    while ( status != WL_CONNECTED)
+    {
+        Serial.print("Attempting to connect to Network named: ");
+        Serial.println(ssid);
 
-  
+        status = WiFi.begin(ssid, pass);
+        delay(5000);
+    }
+
+    printWifiStatus();
+
+    Serial.print("connecting to websocket... ");
+    client.setMessageHandler(handler);
+    while(!client.connect(host, port, websocketUri));
+    Serial.println("OK");
+
+
 }
 
-void loop() {
-  client.monitor();
+void loop()
+{
+    client.monitor();
 }
 
 
-void printWifiStatus() {
-  // print the SSID of the network you're attached to:
-  Serial.print("SSID: ");
-  Serial.println(WiFi.SSID());
+void printWifiStatus()
+{
+    // print the SSID of the network you're attached to:
+    Serial.print("SSID: ");
+    Serial.println(WiFi.SSID());
 
-  // print your WiFi shield's IP address:
-  IPAddress ip = WiFi.localIP();
-  Serial.print("IP Address: ");
-  Serial.println(ip);
+    // print your WiFi shield's IP address:
+    IPAddress ip = WiFi.localIP();
+    Serial.print("IP Address: ");
+    Serial.println(ip);
 
-  // print the received signal strength:
-  long rssi = WiFi.RSSI();
-  Serial.print("signal strength (RSSI):");
-  Serial.print(rssi);
-  Serial.println(" dBm");
-  // print where to go in a browser:
-  Serial.print("To see this page in action, open a browser to http://");
-  Serial.println(ip);
+    // print the received signal strength:
+    long rssi = WiFi.RSSI();
+    Serial.print("signal strength (RSSI):");
+    Serial.print(rssi);
+    Serial.println(" dBm");
+    // print where to go in a browser:
+    Serial.print("To see this page in action, open a browser to http://");
+    Serial.println(ip);
 }
 
 
